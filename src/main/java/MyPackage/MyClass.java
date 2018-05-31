67470b3c0a78c984e2914416070d92e2b61f47bd
[nikdam88] <nikola_damjanovich@yahoo.com>
2018-05-31 12:23:11 +0200

    changes

--- src/main/java/MyPackage/MyClass.java
+++ src/main/java/MyPackage/MyClass.java
@@ -5,6 +5,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("This is fist git projet!");
 		System.out.println("This is second git projet!");
+		System.out.println("This is second git projet!");
 
 	}
 
